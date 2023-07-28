dda4e7519a4b7b556054343fc1662347d5141be8
jayant <jayantahluwalia07@gmail.com>
2023-07-29 01:00:50 +0530

    minor refactor

--- src/constant.ts
+++ src/constant.ts
@@ -65,3 +65,10 @@ export const workContent: workDetail[] = [
     ],
   },
 ];
+
+export const contactDescription = `I am readily available for new opportunities and collaborations, eager
+to embark on inspiring projects. If you have any questions, proposals,
+or just want to connect, don't hesitate to reach out. Let's bring
+innovative ideas to life and make a meaningful impact together.
+Looking forward to hearing from you soon! Feel free to drop me a
+message via Linkdin, email, or any preferred means of communication.`;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,18 +1,12 @@
 import HomeLink from "../components/HomeLink";
+import { contactDescription } from "../constant";
 
 const Contact = () => {
   return (
     <div className="contact-page">
       <h1>Contact</h1>
       <div className="contact-cont">
-        <p>
-          I am readily available for new opportunities and collaborations, eager
-          to embark on inspiring projects. If you have any questions, proposals,
-          or just want to connect, don't hesitate to reach out. Let's bring
-          innovative ideas to life and make a meaningful impact together.
-          Looking forward to hearing from you soon! Feel free to drop me a
-          message via Linkdin, email, or any preferred means of communication.
-        </p>
+        <p>{contactDescription}</p>
         <a href="https://www.linkedin.com/in/jayant-ahluwalia-418a46187/">
           Linkdin
         </a>
